11dda3a886bb7885bf60fbda78a08350b9ef6768
alvarofl <alvarolima837@gmail.com>
2026-05-29 18:22:44 -0300

    codando a parte produtoModel e lançando exceções para não fazer do que o programa não quebre

--- src/br/com/main/burgerking/model/ClienteModel.java
+++ src/br/com/main/burgerking/model/ClienteModel.java
@@ -1,10 +1,55 @@
-
+package model;
 import java.util.UUID;
 
 public class ClienteModel {
-    private UUID id;
+    private final UUID id;
         private String nome;
         private String email;
         private int pontos;
-        
+        public ClienteModel(String nome, String email, int pontos, UUID id) {
+            this.id = UUID.randomUUID();
+this.nome = nome;
+this.email = email;
+this.pontos = pontos;
+        }    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public int getPontos() {
+        return pontos;
+    }
+    public UUID getId() {
+        return id;
+    }
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+        public void setEmail(String email) {
+            this.email = email;
+        }
+        public void setPontos(int pontos) {
+            this.pontos = pontos;
+        }
+        public void adicionarPontos(int pontos) {
+            if (pontos >= 0) {
+                throw new IllegalArgumentException("pontos a adicionar devem serem positivos");
+            }
+            this.pontos += pontos;
+        }
+        public void removerPontos(int pontos) {
+            if (pontos <=0) {
+throw new IllegalArgumentException("os pontos não podem serem negativos");
+            }
+        this.pontos -= pontos;           
+            if (this.pontos<pontos) {
+throw new IllegalArgumentException("pontos isuficientes!");
+            }
+            this.pontos -= pontos;
+}
+
 }
--- src/br/com/main/burgerking/model/Produto.java
+++ src/br/com/main/burgerking/model/Produto.java
@@ -1,9 +1,56 @@
-
+package model;
 import java.util.UUID;
 
 public class Produto {
-    private UUID id;
+    private final UUID id;
     private String nomeProduto;
     private String descricao;
+private double preco;
+    public Produto(UUID id, String nomeProduto, String descricao, double preco) {
+        this.id = UUID.randomUUID();
+        this.nomeProduto = nomeProduto;
+            this.descricao = descricao;
+                this.preco = preco;
+    }
 
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public double getPreco() {
+        return preco;
+    }
+ public void setNomeProduto(String nomeProduto) {
+     this.nomeProduto = nomeProduto;
+ }   
+ public void setPreco(double preco) {
+     this.preco = preco;
+ }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public void produtoInfo(){
+        if (nomeProduto==null) {
+            throw new IllegalArgumentException("o nome do produto não pode ser vasio");
+        }
+        if (descricao==null) {
+            throw new IllegalArgumentException("a descrição do produto não pode ser vasia");
+        }
+        if (preco <= 0) {
+            throw new IllegalArgumentException("o preço do produto deve ser um valor positivo");
+        }
+if (descricao.length()<100) {
+    throw new IllegalArgumentException("a descricação do produto não conter mais 100 caracteres");
+
+}
+
+    }
+    
 }
